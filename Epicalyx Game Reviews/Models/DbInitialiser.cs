207b9f57caf1367deeefac53ddbce17ef508aad5
George Jamieson <ac111480@avcol.school.nz>
2022-03-28 15:15:11 +1300

    attempting to seed data

---
+++ Epicalyx Game Reviews/Models/DbInitialiser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace Epicalyx_Game_Reviews.Models
+{
+    public class DbInitialiser
+    {
+        private readonly Epicalyx_Game_ReviewsContextDb epicalyx_Game_ReviewsContextDb;
+        public DbInitialiser(Epicalyx_Game_ReviewsContextDb epicalyx_Game_ReviewsContextDb)
+        {
+            this.epicalyx_Game_ReviewsContextDb = epicalyx_Game_ReviewsContextDb;
+
+        }
+
+
+
+
+        public void Seed()
+        {
+            if (!epicalyx_Game_ReviewsContextDb.Game.Any())
+            {
+
+                new Game()
+                {
+                    GameName = "Red Dead Redemption 2",
+                    AgeRating = "R16",
+                    Genre = "Action",
+                    Publisher = "Rockstar",
+                    ReleaseYear = 2018
+                };
+
+                epicalyx_Game_ReviewsContextDb.SaveChanges();
+
+
+            }
+        }
+    }
+}
+    
+
+
+
+
+
+
+
